1e7f0c70be5d328871e98b16f308cb424d0ffce0
Isaac Garcia <isgarsi@gmail.com>
2021-11-14 22:27:54 +0100

    Added default values

--- spaceships_invaders/models/models.py
+++ spaceships_invaders/models/models.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 #from odoo import _
 #from odoo.exceptions import Warning
 import logging
+import random
 
 _logger = logging.getLogger(__name__)
 
@@ -12,32 +13,27 @@ class player(models.Model):
      _name = 'spaceships_invaders.player'
      _description = 'Players'
 
-     name = fields.Char(required=True)
-     birth_date = fields.Date(required=True)
-     gender = fields.Char(required=True)
-     enrollment_date = fields.Date()
+     name = fields.Char(required=True, default='player1')
+     birth_date = fields.Date(required=True, default=lambda self: fields.Date.today())
+     gender = fields.Char(required=True, default='M')
+     enrollment_date = fields.Date(default=lambda self: fields.Date.today())
      last_login = fields.Datetime()
-     gold = fields.Integer(required=True)
-     rock = fields.Integer(required=True)
-     metal = fields.Integer(required=True)
+     gold = fields.Integer(required=True, default=100)
+     rock = fields.Integer(required=True, default=300)
+     metal = fields.Integer(required=True, default=200)
      planets = fields.One2many(comodel_name='spaceships_invaders.planet', 
                                 inverse_name='player')
      spaceships = fields.One2many(comodel_name='spaceships_invaders.spaceship', 
                                 inverse_name='player')
 
-     def _get_password(self):
-          for student in self:
-               student.password = "pass aleatorio"
-               #raise Warning(_('Algo ha fallado'))#debugging
-               _logger.debug('\033[94m'+str(student)+'\033[0m')#debugging
-
 class spaceship(models.Model):
      _name = 'spaceships_invaders.spaceship'
      _description = 'Spaceships'
 
-     name = fields.Char(required=True)
-     life = fields.Integer(required=True)
-     is_on_planet = fields.Boolean(required=True)
+     name = fields.Char(required=True, default='Apolo')
+     life = fields.Integer(required=True, default=100)
+     is_on_planet = fields.Boolean(required=True, default=True)
+     
      type = player = fields.Many2one('spaceships_invaders.spaceship_type', 
                                         ondelete='set null', 
                                         help='Tipo de la nave')
@@ -49,23 +45,23 @@ class spaceship_type(models.Model):
     _name = 'spaceships_invaders.spaceship_type'
     _description = 'Spaceship Types'
 
-    name = fields.Char(required=True)
-    gold_needed = fields.Integer(required=True)
-    rock_needed = fields.Integer(required=True)
-    metal_needed = fields.Integer(required=True)
-    damage = fields.Integer(required=True)
-    life = fields.Integer(required=True)
+    name = fields.Char(required=True, default='crucero')
+    gold_needed = fields.Integer(required=True, default=50)
+    rock_needed = fields.Integer(required=True, default=100)
+    metal_needed = fields.Integer(required=True, default=100)
+    damage = fields.Integer(required=True, default=lambda self: random.randint(20,100), readonly=True)
+    life = fields.Integer(required=True,  default=lambda self: random.randint(50,200), readonly=True)
 
 
 class planet(models.Model):
      _name = 'spaceships_invaders.planet'
      _description = 'Planets'
 
-     name = fields.Char(required=True)
-     life = fields.Integer(required=True)
-     quantity_gold_hour = fields.Integer(required=True)
-     quantity_rock_hour = fields.Integer(required=True)
-     quantity_metal_hour = fields.Integer(required=True)
+     name = fields.Char(required=True, default='namek')
+     life = fields.Integer(required=True, default=lambda self: random.randint(300,1000), readonly=True)
+     quantity_gold_hour = fields.Integer(required=True, default=lambda s: s.life/100, readonly=True)
+     quantity_rock_hour = fields.Integer(required=True, default=lambda s: s.life/10, readonly=True)
+     quantity_metal_hour = fields.Integer(required=True, default=lambda s: s.life/50, readonly=True)
 
      player = fields.Many2one('spaceships_invaders.player', ondelete='set null', help='Jugador al que pertenece')
 
